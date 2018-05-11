9863d1849f998c5fea27cd0c1ba0de00c7880ef6
artem gruden <artemxgruden@gmail.com>
2018-05-11 11:02:32 +0300

    mod: load dynamic imported components method

--- functions/loadDynamicImportedComponents.mjs
+++ functions/loadDynamicImportedComponents.mjs
@@ -2,7 +2,11 @@ export default async components => {
 
   if ( components && components.length > 0 ){
     for ( const c of components ){
-      await c.load()
+
+      if ( c && c.load ){
+        await c.load()
+      }
+      
     }
   }
 
